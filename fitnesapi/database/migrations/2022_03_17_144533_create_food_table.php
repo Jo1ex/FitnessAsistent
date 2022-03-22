8942d2b58598fc4b093e53777cea32aea05f79d6
patrik <patrik.pauman@sers.si>
2022-03-22 12:45:58 +0100

    dodal databayo

---
+++ fitnesapi/database/migrations/2022_03_17_144533_create_food_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('food', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('food');
+            $table->string('calories');
+            $table->string('proteins');
+            $table->string('fats');
+            $table->string('carbon');
+            $table->foreign('user_id')
+            ->on('users')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('food');
+    }
+};
