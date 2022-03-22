44536aabc7e03494712d028bcbffe3acf6d2a159
Jo1ex <99878899+Jo1ex@users.noreply.github.com>
2022-03-22 08:37:41 +0100

    Update HomeView.vue

--- fitnesweb/src/views/HomeView.vue
+++ fitnesweb/src/views/HomeView.vue
@@ -1,136 +1,54 @@
 <template>
   <v-app id="inspire">
-    <v-content>
+    
       <v-container class="fill-height" fluid>
         <v-row align="center" justify="center">
           <v-col cols="12" sm="8" md="8">
-            <v-card class="elevation-12">
-              <v-window v-model="step">
+            
+              <v-window v-model="step" align="center" justify="">
                 <v-window-item :value="1">
-                  <v-row>
                     <v-col cols="12" md="8">
                       <v-card-text class="mt-12">
                         <h1
-                          class="text-center display-2 teal--text text--accent-3"
-                        >Prijava v FitnesAsistent</h1>
-                        <div class="text-center mt-4">
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-facebook-f</v-icon>
-                          </v-btn>
-
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-google-plus-g</v-icon>
-                          </v-btn>
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-linkedin-in</v-icon>
-                          </v-btn>
-                        </div>
-                        <h4 class="text-center mt-4">Vpisite svoj email za registracijo</h4>
+                          color=#00A99D class="text-center display-2 teal--text text--accent-3"
+                        >Dobrodosli!</h1>
+                        
+                        <h4 class="text-center mt-4">Vpisite svoj email za prijavo</h4>
                         <v-form>
                           <v-text-field
-                            label="Email"
+                            label="Email naslov*"
                             name="Email"
-                            prepend-icon="email"
+                            prepend-icon="e"
                             type="text"
-                            color="teal accent-3"
+                            color="teal accent-2"
                           />
 
                           <v-text-field
                             id="Geslo"
-                            label="Geslo"
+                            label="Geslo*"
                             name="Geslo"
-                            prepend-icon="lock"
+                            prepend-icon="g"
                             type="Geslo"
-                            color="teal accent-3"
+                            color="teal accent-2"
                           />
                         </v-form>
-                        <h3 class="text-center mt-4">Pozabljeno geslo ?</h3>
+                        <v-btn text x-small>Ali ste pozabili geslo?</v-btn>
+                        
+                        <v-btn text x-small>Registracija</v-btn>
                       </v-card-text>
                       <div class="text-center mt-3">
-                        <v-btn rounded color="teal accent-3" dark>PRIJAVA</v-btn>
+                        <v-btn color="#00A99D" elevation="5" outlined x-large bold >Prijavi se</v-btn>
                       </div>
                     </v-col>
-                    <v-col cols="12" md="4" class="teal accent-3">
-                      <v-card-text class="white--text mt-12">
-                        <h1 class="text-center display-1">Dobrodosel!</h1>
-                        <h5
-                          class="text-center"
-                        >Da bi ostali povezani s nami prosim vpisite iskane podatke</h5>
-                      </v-card-text>
-                      <div class="text-center">
-                        <v-btn rounded outlined dark @click="step++">REGISTRACIJA</v-btn>
-                      </div>
-                    </v-col>
-                  </v-row>
+                    
                 </v-window-item>
-                <v-window-item :value="2">
-                  <v-row class="fill-height">
-                    <v-col cols="12" md="4" class="teal accent-3">
-                      <v-card-text class="white--text mt-12">
-                        <h1 class="text-center display-1">Dobrodosel!</h1>
-                        <h5
-                          class="text-center"
-                        >Da bi ostali povezani s nami prosim vpisite iskane podatke</h5>
-                      </v-card-text>
-                      <div class="text-center">
-                        <v-btn rounded outlined dark @click="step--">Prijava</v-btn>
-                      </div>
-                    </v-col>
-
-                    <v-col cols="12" md="8">
-                      <v-card-text class="mt-12">
-                        <h1 class="text-center display-2 teal--text text--accent-3">Izdelaj racun</h1>
-                        <div class="text-center mt-4">
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-facebook-f</v-icon>
-                          </v-btn>
-
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-google-plus-g</v-icon>
-                          </v-btn>
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-linkedin-in</v-icon>
-                          </v-btn>
-                        </div>
-                        <h4 class="text-center mt-4">Vpisi svoj email za registracijo</h4>
-                        <v-form>
-                          <v-text-field
-                            label="Ime"
-                            name="Ime"
-                            prepend-icon="person"
-                            type="text"
-                            color="teal accent-3"
-                          />
-                          <v-text-field
-                            label="Email"
-                            name="Email"
-                            prepend-icon="email"
-                            type="text"
-                            color="teal accent-3"
-                          />
-
-                          <v-text-field
-                            id="Geslo"
-                            label="Geslo"
-                            name="Geslo"
-                            prepend-icon="lock"
-                            type="Geslo"
-                            color="teal accent-3"
-                          />
-                        </v-form>
-                      </v-card-text>
-                      <div class="text-center mt-n5">
-                        <v-btn rounded color="teal accent-3" dark>REGISTRACIJA</v-btn>
-                      </div>
-                    </v-col>
-                  </v-row>
-                </v-window-item>
+                
               </v-window>
-            </v-card>
+            
           </v-col>
         </v-row>
       </v-container>
-    </v-content>
+    
   </v-app>
 </template>
 
