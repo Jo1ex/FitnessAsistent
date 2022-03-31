f417b49719ebf4ceb855f2e79163a4a310070bda
Jo1ex <99878899+Jo1ex@users.noreply.github.com>
2022-03-31 11:08:18 +0200

    Update main.js

--- fitnesweb/src/main.js
+++ fitnesweb/src/main.js
@@ -1,7 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import router from './router';
+import App from './App.vue';
 import vuetify from './plugins/vuetify';
+
+
+
+
 
 Vue.config.productionTip = false
 
