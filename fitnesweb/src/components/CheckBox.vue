0c1b836fa68b6238e3e1fa6df5095a8e8e96eae2
Jo1ex <99878899+Jo1ex@users.noreply.github.com>
2022-05-06 10:37:37 +0200

    Add files via upload

---
+++ fitnesweb/src/components/CheckBox.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card-title>
+          <v-list-item-avatar color="grey darken-3">
+            <v-icon dark>mdi-account</v-icon> 
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title>Jolex</v-list-item-title>
+            </v-list-item-content>
+            <v-checkbox
+              v-model="selected"
+            ></v-checkbox>
+        </v-card-title>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
